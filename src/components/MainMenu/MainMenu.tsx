97da384f69dfdcc463c79be6a0b579f8b0748c3d
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-12-26 14:00:31 +0100

    added state to the main menu component

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,19 +1,59 @@
 import React, { Component } from 'react'
 import { Container, Nav } from 'react-bootstrap'
 
-export class MainMenu extends Component {
+export class MainMenuItem {
+    text: string = '';
+    link: string = '#';
+
+    constructor(text: string, link:string) {
+        this.text = text;
+        this.link = link;
+    }
+}
+
+interface MainMenuProperties {
+    items: MainMenuItem[];
+}
+
+interface MainMenuState {
+    items: MainMenuItem[];
+
+}
+
+export class MainMenu extends Component<MainMenuProperties> {
+    state: MainMenuState;
+
+    constructor(props: Readonly<MainMenuProperties>) {
+        super(props);
+
+        this.state = {
+            items: props.items,
+        };
+    }
+
+    setItems(items: MainMenuItem[]) {
+        this.setState({
+            items: items,
+        });
+    }
+
     render() {
         return (
             <Container>
                 <Nav variant="tabs">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/">Contact</Nav.Link>
-                    <Nav.Link href="/">Log in</Nav.Link>
+                    { this.state.items.map(this.makeNavLink) }
                 </Nav>
             </Container>
         )
     }
+
+    private makeNavLink(item: MainMenuItem) {
+        return (
+            <Nav.Link href={ item.link }>
+                { item.text }
+            </Nav.Link>
+        );
+    }
 }
 
-export default MainMenu
 
